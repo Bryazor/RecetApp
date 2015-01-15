b0b093151ca0e5299e8dacc468e94721bb3f6fff
Braulio <brauliogc@hotmail.es>
2015-01-15 10:46:53 +0000

    CategoriaService y MedidaService acabados

--- src/dad/recetapp/services/impl/CategoriasService.java
+++ src/dad/recetapp/services/impl/CategoriasService.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import dad.recetapp.db.BaseDatos;
@@ -20,17 +21,16 @@ public class CategoriasService implements ICategoriaServices {
 	@Override
 	public void crearCategoria(CategoriaItem categoria) throws ServiceException {
 		try {
-			
+
 			Connection conn = BaseDatos.getConnection();
 
-			PreparedStatement statment = conn
-					.prepareStatement("insert into categorias (descripcion) values (?)");
+			PreparedStatement statment = conn.prepareStatement("insert into categorias (descripcion) values (?)");
 			statment.setString(1, categoria.getDescripcion());
 			statment.executeUpdate();
 			statment.close();
 		} catch (SQLException e) {
 			throw new ServiceException("Error al crear la categoria '"
-					+ categoria.getDescripcion(), e);
+					+ categoria.getDescripcion() + "' ", e);
 		} catch (NullPointerException e) {
 			throw new ServiceException(
 					"Error al crear la categoria: La categoria no puede ser nula");
@@ -38,32 +38,63 @@ public class CategoriasService implements ICategoriaServices {
 	}
 
 	@Override
-	public void modificarCategoria(CategoriaItem categoria)
-			throws ServiceException {
-		// TODO Auto-generated method stub
-
+	public void modificarCategoria(CategoriaItem categoria) throws ServiceException {
+		try {
+			Connection conn = BaseDatos.getConnection();
+			PreparedStatement statment = conn.prepareStatement("update categorias set descripcion =? where id = ?");
+			statment.setString(1, categoria.getDescripcion());
+			statment.setLong(2, categoria.getId());
+			statment.executeUpdate();
+			statment.close();
+		} catch (SQLException e) {
+			throw new ServiceException("Error al modificar la categoria '"
+					+ categoria.getDescripcion() + "' ", e);
+		} catch (NullPointerException e) {
+			throw new ServiceException("Error al modificar la categoria: La categoria no puede ser nula");
+		}	
 	}
 
 	@Override
 	public void eliminarCategoria(Long id) throws ServiceException {
-		// TODO Auto-generated method stub
-
+		try {
+			Connection conn = BaseDatos.getConnection();
+			PreparedStatement statment = conn.prepareStatement("delete from categorias where id = ?");
+			statment.setLong(1, id);
+			statment.executeUpdate();
+			statment.close();
+		} catch (SQLException e) {
+			throw new ServiceException("Error al eliminar la categoria con id '"
+					+ id + "' ", e);
+		} catch (NullPointerException e) {
+			throw new ServiceException("Error al eliminar la categoria: Debe especificar el identificador");
+		}
 	}
 
 	@Override
 	public List<CategoriaItem> listarCategorias() throws ServiceException {
-		// TODO Auto-generated method stub
-		return null;
+		List<CategoriaItem> listaCategorias = new ArrayList<CategoriaItem>();
+		CategoriaItem categoria = null;
+		try {
+			Connection conn = BaseDatos.getConnection();
+			PreparedStatement statment = conn.prepareStatement("select * from categorias");
+			ResultSet rs = statment.executeQuery();
+			while (rs.next()) {
+				categoria = new CategoriaItem();
+				categoria.setId(rs.getLong("id"));
+				categoria.setDescripcion(rs.getString("descripcion"));
+				listaCategorias.add(categoria);
+			}
+			statment.close();
+		} catch (SQLException e) {
+			throw new ServiceException("Error al recuperar las categorias", e);
+		}
+		return listaCategorias;
 	}
 
 	@Override
 	public CategoriaItem obtenerCategoria(Long id) throws ServiceException {
 		CategoriaItem categoria = null;
 		try {
-			if (id == null){
-				throw new ServiceException("Error al recuperar la categoria: Debe especificar el identificador");
-			}
-
 			Connection conn = BaseDatos.getConnection();
 			PreparedStatement statment = conn.prepareStatement("select * from categorias when id = ?");
 			statment.setLong(1, id);
@@ -76,6 +107,8 @@ public class CategoriasService implements ICategoriaServices {
 			statment.close();
 		} catch (SQLException e) {
 			throw new ServiceException("Error al recuperar la categoria con ID: '" + id + "'", e);
+		}catch (NullPointerException e) {
+			throw new ServiceException("Error al recuperar la categoria: Debe especificar el identificador");
 		}
 		return categoria;
 	}
--- src/dad/recetapp/services/impl/MedidasService.java
+++ src/dad/recetapp/services/impl/MedidasService.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import dad.recetapp.db.BaseDatos;
@@ -19,51 +20,78 @@ public class MedidasService implements IMedidasService {
 
 	@Override
 	public void crearMedida(MedidaItem medida) throws ServiceException {
-		try {
-			
+		try {			
 			Connection conn = BaseDatos.getConnection();
-
-			PreparedStatement statment = conn
-					.prepareStatement("insert into medidas (nombre, abreviatura) values (?, ?)");
+			PreparedStatement statment = conn.prepareStatement("insert into medidas (nombre, abreviatura) values (?, ?)");
 			statment.setString(1, medida.getNombre());
 			statment.setString(2, medida.getAbreviatura());
 			statment.executeUpdate();
 			statment.close();
 		} catch (SQLException e) {
-			throw new ServiceException("Error al crear la medida '"
-					+ medida.getNombre(), e);
+			throw new ServiceException("Error al crear la medida '" + medida.getNombre() + "'", e);
 		} catch (NullPointerException e) {
-			throw new ServiceException(
-					"Error al crear la medida: La categoria no puede ser nula");
+			throw new ServiceException("Error al crear la medida: La medida no puede ser nula");
 		}
 	}
 
 	@Override
 	public void modificarMedida(MedidaItem medida) throws ServiceException {
-		// TODO Auto-generated method stub
-
+		try {
+			Connection conn = BaseDatos.getConnection();
+			PreparedStatement statment = conn.prepareStatement("update medidas set nombre =?, abreviatura=? where id = ?");
+			statment.setString(1, medida.getNombre());
+			statment.setString(2, medida.getAbreviatura());
+			statment.setLong(3, medida.getId());
+			statment.executeUpdate();
+			statment.close();
+		} catch (SQLException e) {
+			throw new ServiceException("Error al modificar la medida '" + medida.getNombre() + "' ", e);
+		} catch (NullPointerException e) {
+			throw new ServiceException("Error al modificar la medida: La medida no puede ser nula");
+		}
 	}
 
 	@Override
 	public void eliminarMedida(Long id) throws ServiceException {
-		// TODO Auto-generated method stub
-
+		try {
+			Connection conn = BaseDatos.getConnection();
+			PreparedStatement statment = conn.prepareStatement("delete from medidas where id = ?");
+			statment.setLong(1, id);
+			statment.executeUpdate();
+			statment.close();
+		} catch (SQLException e) {
+			throw new ServiceException("Error al eliminar la medida con id '" + id + "' ", e);
+		} catch (NullPointerException e) {
+			throw new ServiceException("Error al eliminar la medida: Debe especificar el identificador");
+		}
 	}
 
 	@Override
 	public List<MedidaItem> listarMedidas() throws ServiceException {
-		// TODO Auto-generated method stub
-		return null;
+		List<MedidaItem> listaMedidas = new ArrayList<MedidaItem>();
+		MedidaItem medida = null;
+		try {
+			Connection conn = BaseDatos.getConnection();
+			PreparedStatement statment = conn.prepareStatement("select * from categorias");
+			ResultSet rs = statment.executeQuery();
+			while (rs.next()) {
+				medida = new MedidaItem();
+				medida.setId(rs.getLong("id"));
+				medida.setNombre(rs.getString("nombre"));
+				medida.setAbreviatura(rs.getString("abreviatura"));
+				listaMedidas.add(medida);
+			}
+			statment.close();
+		} catch (SQLException e) {
+			throw new ServiceException("Error al recuperar las medidas", e);
+		}
+		return listaMedidas;
 	}
 
 	@Override
 	public MedidaItem obtenerMedida(Long id) throws ServiceException {
 		MedidaItem medida = null;
 		try {
-			if (id == null){
-				throw new ServiceException("Error al recuperar la medida: Debe especificar el identificador");
-			}
-
 			Connection conn = BaseDatos.getConnection();
 			PreparedStatement statment = conn.prepareStatement("select * from categorias when id = ?");
 			statment.setLong(1, id);
@@ -77,6 +105,8 @@ public class MedidasService implements IMedidasService {
 			statment.close();
 		} catch (SQLException e) {
 			throw new ServiceException("Error al recuperar la categoria con ID: '" + id + "'", e);
+		} catch (NullPointerException e) {
+			throw new ServiceException("Error al recuperar la medida: Debe especificar el identificador");
 		}
 		return medida;
 	}
